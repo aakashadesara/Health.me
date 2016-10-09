f06a63a070ef23c192c4bea85a2c81056fed31a6
Aakash Adesara <aakash.adesara@gmail.com>
2016-10-09 11:29:05 -0400

    last minute copping

--- Mental Health/datasets/bipolar.m
+++ Mental Health/datasets/bipolar.m
@@ -1,4 +1,4 @@
-t = load('schitzophrenia.dat');
+t = load('bipolar.dat');
 x = t'(1,:);
 y = t'(2,:);
 
@@ -6,7 +6,7 @@ p = polyfit(x, log(y), 3)
 
 polyout(p, "x");
 
-a = plot(x, log(y), "*", x, polyval(p, x), "+");
+a = plot(x, (y), "*", x, polyval(p, x), "+");
 xlabel("sleep");
 ylabel("sch. level");
 legend("Data points", "Linear Fit");
--- Mental Health/datasets/depression.m
+++ Mental Health/datasets/depression.m
@@ -1,8 +1,8 @@
-t = load('schitzophrenia.dat');
+t = load('depression.dat');
 x = t'(1,:);
 y = t'(2,:);
 
-p = polyfit(x, log(y), 3)
+p = polyfit(x, log(y), 5)
 
 polyout(p, "x");
 
